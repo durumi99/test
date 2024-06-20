7bebac6efc040390039961b2a8ff60748d019f88
durumi99 <kms990615@naver.com>
2024-06-20 21:14:52 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ import { WINNING_COMBINATIONS } from './winning-combinations';
 import GameOver from './GameOver';
 
 const PLAYERS = {
-	X: 'Branch Dev',
-	O: '6 commits',
+	X: 'Branch Dev2',
+	O: 'commits',
 };
-const tmp = 'DEV2-1';
 
 const INITAIL_GAME_BOARD = [
 	[null, null, null],
