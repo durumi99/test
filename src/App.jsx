7cbd7a6a596c42fa36fe7a869420ffb89e8756c9
durumi99 <kms990615@naver.com>
2024-06-20 20:53:49 +0900

    4th commit by dev

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,8 @@ import { WINNING_COMBINATIONS } from './winning-combinations';
 import GameOver from './GameOver';
 
 const PLAYERS = {
-<<<<<<< HEAD
 	X: 'Branch Dev',
-	O: 'third commit',
-=======
-	X: 'Branch Dev2',
-	O: 'initial commit',
->>>>>>> d8ef94a (initial commit by dev2)
+	O: '4 commit',
 };
 
 const INITAIL_GAME_BOARD = [
